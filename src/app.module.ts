2cb1ed246144b9d50470c4ed0a7ffbeb8a5d5619
Lucas Guilha <medowlucas@gmail.com>
2022-04-05 21:29:02 -0300

    adicionando as primeiras entidades de relacionamento

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ObraModule } from './obra/obra.module';
+import { VeiculoModule } from './veiculo/veiculo.module';
+import { UsuarioModule } from './usuario/usuario.module';
+import { ClienteModule } from './cliente/cliente.module';
 
 @Module({
-  imports: [],
+  imports: [ObraModule, VeiculoModule, UsuarioModule, ClienteModule],
   controllers: [AppController],
   providers: [AppService],
 })
